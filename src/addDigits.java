d58cbffebbf9c0df02710756952144f5d7c0500b
Michael Zhang <heronalps@gmail.com>
2016-06-28 22:27:51 -0400

    Commit_20160628_02

--- src/addDigits.java
+++ src/addDigits.java
@@ -2,7 +2,7 @@
  * Created by michael.zhang on 6/28/2016.
  */
 public class addDigits {
-    public static int addDigits(int num){
+    public static int addDigits(int num) {
         if (num < 10) {
             return num;
         }
@@ -18,7 +18,22 @@ public class addDigits {
         return result;
     }
 
+    public static int addDigits2(int num) {
+        int counter = 0;
+        while(num > 0) {
+            int digit = num % 10;
+            num = num / 10;
+            counter += digit;
+            if (num == 0 && counter > 9) {
+                num = counter;
+                counter = 0;
+            }
+        }
+        return counter;
+    }
+
     public static void main(String[] args) {
         System.out.println(addDigits(456));
+        System.out.println(addDigits2(456));
     }
 }
