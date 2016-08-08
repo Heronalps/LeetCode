55b92d2f8d52ed2150f4e531da0442dd74c48e5b
Heronalps <heronalps@gmail.com>
2016-08-08 16:42:26 -0400

    Commit_20160808

--- src/isAnagram.java
+++ src/isAnagram.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Created by michael.zhang on 8/4/2016.
@@ -48,8 +49,42 @@ public class isAnagram {
         return true;
     }
 
+    public static boolean isAnagram3(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> counter = new HashMap<Character, Integer>();
+        char[] source = s.toCharArray();
+        char[] target = t.toCharArray();
+
+        for (int i = 0; i < source.length; i++) {
+            if (counter.containsKey(source[i])) {
+                counter.put(source[i], (counter.get(source[i]) + 1));
+            } else {
+                counter.put(source[i], 1);
+            }
+        }
+
+        for (int j = 0; j < target.length; j++) {
+            if (!counter.containsKey(target[j]) || counter.get(target[j]) == 0){
+
+                //这个判断条件是正确的。因为之前已经确认两个String的长度一致。
+                // 如果Target中某个字母少于Source而没有被减到零，则必有另一个字母被减到小于零。
+
+                return false;
+            } else {
+                counter.put(target[j], (counter.get(target[j]) - 1));
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         System.out.println(isAnagram("bad", "bde"));
         System.out.println(isAnagram2("bad", "bde"));
+        System.out.println(isAnagram3("beed", "bedd"));
+
     }
 }
