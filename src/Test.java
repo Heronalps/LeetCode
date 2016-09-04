2d8654b23ba6f781a89474528f00adf64e086a28
Michael Zhang <heronalps@gmail.com>
2016-09-03 23:25:12 -0400

    Commit_20160903

--- src/Test.java
+++ src/Test.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by michael.zhang on 8/11/2016.
  */
 public class Test {
     public static void main(String[] args) {
-        int[] result = Arrays.copyOfRange(new int[] {0,1,2,3},1,4);
-        System.out.println(result.length);
+        Integer[] result = {0,1,2,3};
+        Arrays.sort(result, Collections.reverseOrder());
         for (int i = 0; i < result.length; i++) {
             System.out.println(result[i]);
         }
