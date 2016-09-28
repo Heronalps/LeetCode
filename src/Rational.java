5e43560a6703bc5f5324b7697d73871ef15d841f
Michael Zhang <heronalps@gmail.com>
2016-09-28 15:17:05 -0700

    Commit_20160928

---
+++ src/Rational.java
@@ -0,0 +1,20 @@
+/**
+ * Created by michaelzhang on 9/26/16.
+ */
+
+public class Rational {
+
+    private int num;
+    private int denom;
+
+    public Rational() {
+        this.num = 1;
+        this.denom = 1;
+    }
+
+    public Rational(int num, int denom) {
+        this.num = num;
+        this.denom = denom;
+    }
+
+}
