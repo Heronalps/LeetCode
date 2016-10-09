86bd80c50fb98d12b3f628de285ac0dc44065ee0
michaelzhang <heronalps@gmail.com>
2016-10-08 19:01:15 -0700

    Commit_20161008

--- src/BackPack.java
+++ src/BackPack.java
@@ -2,4 +2,32 @@
  * Created by michaelzhang on 10/5/16.
  */
 public class BackPack {
+    public static int backPack(int m, int[] A) {
+        boolean f[][] = new boolean[A.length + 1][m + 1];
+        for (int i = 0; i <= A.length; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i][j] = false;
+            }
+        }
+        f[0][0] = true;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j <= m; j++) {
+                f[i + 1][j] = f[i][j];
+                if (j >= A[i] && f[i][j - A[i]]) {
+                    f[i + 1][j] = true;
+                }
+            } // for j
+        } // for i
+
+        for (int i = m; i >= 0; i--) {
+            if (f[A.length][i]) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(backPack(11, new int[] {2,7,5,3})); //answer = 10
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -5,13 +5,26 @@ import java.util.Collections;
  * Created by michael.zhang on 8/11/2016.
  */
 public class Test {
-    public static void main(String[] args) {
-        /*Integer[] result = {0,1,2,3};
-        Arrays.sort(result, Collections.reverseOrder());
-        for (int i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
+
+    public static int degreeCoeffsHighToLow(int [] coeffsHighToLow) {
+
+        // start at the low end, where leading zeros might be,
+        // and work our way down to the first non zero, and return
+        // the length, minus that coefficient.
+        // e.g. {0, 0, 0, 0, 10, 0, 20, 0, 30, 0 } => 5
+
+        int degree = coeffsHighToLow.length - 1;
+        for (int i=0; i<coeffsHighToLow.length; i++) {
+            if (coeffsHighToLow[i]!=0) {
+                return degree;
+            }
+            degree--;
         }
-    }*/
-        System.out.println(-5 % 6);
+        return 0;
     }
+    public static void main(String[] args) {
+
+        System.out.println(degreeCoeffsHighToLow(new int[] {0, 0, 0, 0, 10, 0, 20, 0, 30, 0 }));
+    }
+
 }
